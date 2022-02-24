08bfd3999ceeba5a52eebcfff28f50ebee033f85
Suhail_Ali_M <suhail.ali8@gmail.com>
2022-02-24 11:04:22 +0530

    handled backstact after order success naviagtion

--- app/src/main/java/com/suhail/simpleecommerceapp/order/OrderSuccess.kt
+++ app/src/main/java/com/suhail/simpleecommerceapp/order/OrderSuccess.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import com.suhail.simpleecommerceapp.R
 import com.suhail.simpleecommerceapp.SCREEN_HOME
+import com.suhail.simpleecommerceapp.Screen
 
 @Composable
 fun OrderSuccess(navController: NavHostController) {
@@ -23,7 +24,9 @@ fun OrderSuccess(navController: NavHostController) {
         Text(text = "Congratulations", fontSize = 24.sp, modifier = Modifier.testTag("testTagCongrats"))
         Text(text = "Your order has been success placed")
         Spacer(modifier = Modifier.height(16.dp))
-        Button(onClick = { navController.navigate(SCREEN_HOME) }) {
+        Button(onClick = { navController.navigate(SCREEN_HOME) {
+            popUpTo(Screen.Home.route)
+        } }) {
             Text(text = "Okay")
         }
     }
